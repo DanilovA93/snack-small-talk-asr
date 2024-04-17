57dbffb9ed9a86c111def235d73e2820aff91023
DanilovA93 <danilovanton1993@mail.ru>
2024-04-17 20:25:21 +0300

    test

--- start.sh
+++ start.sh
@@ -2,6 +2,20 @@
 sudo apt-get update
 sudo apt install python3-pip
 pip3 install git+https://github.com/NVIDIA/NeMo.git
+pip3 install hydra-core --upgrade
+pip3 install pytorch_lightning
+pip3 install librosa
+pip3 install sentencepiece
+pip3 install pandas
+pip3 install inflect
+pip3 install lhotse
+pip3 install editdistance
+pip3 install jiwer
+pip3 install pyannote.core
+pip3 install webdataset
+pip3 install datasets
+pip3 install pyannote.metrics
+pip3 install IPython
 sudo touch ./output.log
 sudo chmod 777 ./output.log
 sudo chmod 733 ./server.py
