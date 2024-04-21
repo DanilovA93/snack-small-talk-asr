d615289c81d13618293edd532d05a6c23b241bb7
DanilovA93 <danilovanton1993@mail.ru>
2024-04-21 16:30:01 +0300

    add req

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/zsh
 sudo apt-get update
 sudo apt install python3-pip
+pip3 -r requirements.txt
 pip3 install git+https://github.com/NVIDIA/NeMo.git
 pip3 install hydra-core --upgrade
-pip3 -r requirements.txt
 touch ./output.log
 chmod 777 ./output.log
 chmod 733 ./server.py
